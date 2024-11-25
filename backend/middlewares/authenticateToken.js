68e96a814b85f1b67f6a14ddfffa206858b4cc1a
KLmk1 <klimakarov@gmail.com>
2024-11-25 18:37:31 +0400

    xzche

---
+++ backend/middlewares/authenticateToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.header('Authorization')?.split(' ')[1]; // Берем токен из заголовка
+
+  if (!token) {
+    return res.status(403).json({ error: 'Access denied' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Invalid token' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
